6b0ee6c9b19b8649bfd0d5ce93d542e50d2b5f58
Alex <pyrius@gmail.com>
2013-08-02 22:58:41 +0200

    Cleaning the code

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import os, sys
 
-'''
-if sys.argv[1]:
-	print "1st arg " + sys.argv[1]
-if sys.argv[2]:
-	print "2nd arg " + sys.argv[1]
-
-print len(dir(sys))
-'''
-
 path = os.getcwd()
 dir = sys.argv[1]
 print path
