ef650382ae2a1c09e1c53d5e621c8d81ca178405
Alex <pyrius@gmail.com>
2013-08-05 11:18:47 +0200

    added file_open function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+'''
+	Algorithm:
+	1. Get the directory
+	2. Switch to the directory
+	3. List the files
+	4. Get the filename
+	5. If the filename is '..' go higher and get back to step 1
+	6. If the filename is 'directory' go to the directory and get back to step 1
+	7. If the filename is a file - return txt
+'''
 import os, sys
 
 def more(text,numlines=5):
@@ -9,23 +19,59 @@ def more(text,numlines=5):
 		if lines and raw_input('More? [Y|y]: ') not in ["Y", "y", ""]: break
 		print '========================================'
 
-
-def flopen():
-	pt = raw_input('Enter the directory: ')
-	#pth = sys.argv[1]
-	#os.chdir(sys.argv[1])
-	os.chdir(pt)
+def init():
+	os.chdir('/')
 	print os.getcwd()
-	#print os.listdir(os.getcwd())
-	print os.listdir(pt)
-	fname = raw_input('Enter the filename: ')
-	txt = open(pt+'/'+fname).read()
+	pth = os.getcwd()
+	print os.listdir(pth)
+	lst = os.listdir(pth)
+
+	for item in lst:
+		if os.path.isfile(item):
+			print item + " - a file"	
+		else:
+			print item + " - a folder"	
+
+
+def file_name():
+	pt = raw_input('Enter the directory: ')
+	fpt = os.getcwd()+'/'+pt
+	print "fpt is (before while) "+ fpt
+	print "fpt is a directory - " + os.path.isdir(fpt)
+	while os.path.isdir(fpt):
+		os.chdir(fpt)
+		print os.getcwd()
+		print "fpt is " + fpt
+		print os.listdir(fpt)
+		lst = os.listdir(fpt)
+
+		for item in lst:
+			if os.path.isfile(item):
+				print item + " - a file"
+			else:
+				print item + " - a folder"	
+	
+		pt = raw_input('Enter the directory or filename: ')
+		fpt = fpt + "/" + pt
+
+		if pt == '..':
+			print "not ready"
+		elif os.path.isdir(fpt+"/"+pt):
+			fpt = fpt + "/" + pt
+		else:
+			return fpt
+
+
+def file_open(fnm):
+	txt = open(fnm).read()
 	return txt
 
 
 if __name__ == '__main__':
+	init()
 #	import sys, os
-	text = flopen()
+	fnm = file_name()
+	text = file_open(fnm)
 	more(text, 5)
 	'''
 	more(open(sys.argv[1]+'/'+sys.argv[2]).read(), 5)
