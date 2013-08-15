3a6a6b418a738f199e6e27abd13ceeb4485a16a2
Alex <pyrius@gmail.com>
2013-08-15 14:25:55 +0200

    Working version

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-'''
-	Algorithm:
-	1. Get the directory
-	2. Switch to the directory
-	3. List the files
-	4. Get the filename
-	5. If the filename is '..' go higher and get back to step 1
-	6. If the filename is 'directory' go to the directory and get back to step 1
-	7. If the filename is a file - return txt
-'''
 import os, sys
+
+def file_open(fnm):
+	txt = open(fnm).read()
+	return txt
 
 def more(text,numlines=5):
 	lines = text.splitlines()
@@ -21,59 +15,51 @@ def more(text,numlines=5):
 
 def init():
 	os.chdir('/')
-	print os.getcwd()
 	pth = os.getcwd()
-	print os.listdir(pth)
 	lst = os.listdir(pth)
-
+	print pth
+	print lst
+	'''
 	for item in lst:
 		if os.path.isfile(item):
 			print item + " - a file"	
 		else:
 			print item + " - a folder"	
+	'''
+	return pth
 
-
-def file_name():
-	pt = raw_input('Enter the directory: ')
-	fpt = os.getcwd()+'/'+pt
+def file_name(fpt):
+	pt = raw_input('Enter the directory or filename: ')
+	fpt = fpt+'/'+pt
 	print "fpt is (before while) "+ fpt
-	print "fpt is a directory - " + os.path.isdir(fpt)
-	while os.path.isdir(fpt):
+	print "fpt is a directory - " + str(os.path.isdir(fpt))
+	while True:
 		os.chdir(fpt)
 		print os.getcwd()
 		print "fpt is " + fpt
 		print os.listdir(fpt)
 		lst = os.listdir(fpt)
-
 		for item in lst:
 			if os.path.isfile(item):
 				print item + " - a file"
 			else:
 				print item + " - a folder"	
-	
 		pt = raw_input('Enter the directory or filename: ')
-		fpt = fpt + "/" + pt
-
+		print pt
+		#fpt = fpt + "/" + pt
+		print fpt
 		if pt == '..':
 			print "not ready"
-		elif os.path.isdir(fpt+"/"+pt):
+		elif os.path.isdir(fpt + "/" + pt):
 			fpt = fpt + "/" + pt
 		else:
-			return fpt
+			return fpt + "/" + pt
 
 
-def file_open(fnm):
-	txt = open(fnm).read()
-	return txt
 
 
 if __name__ == '__main__':
-	init()
-#	import sys, os
-	fnm = file_name()
+	dpth = init()
+	fnm = file_name(dpth)
 	text = file_open(fnm)
 	more(text, 5)
-	'''
-	more(open(sys.argv[1]+'/'+sys.argv[2]).read(), 5)
-	'''
-	
